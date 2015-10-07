da74f11560f34b699dfe16113772252f272f3837
Erhan Kesken <ekesken@users.noreply.github.com>
2015-10-08 01:13:49 +0300

    made security.keyFile setting optional

--- 2.6/env-entrypoint.sh
+++ 2.6/env-entrypoint.sh
@@ -7,8 +7,16 @@ storage:
 replication:
   replSetName: $REPLICATION_SET_NAME
 
-security:
-  keyFile: /etc/mongodb/ebrandvalue-mongodb-keyfile
 EOF
+
+if [$KEY_FILE_CONTENT != '']; then
+  mkdir -p /etc/mongod-keyfile
+  echo $KEY_FILE_CONTENT | tr ' ' '\n' > /etc/mongod-keyfile
+  chmod 600 /etc/mongod-keyfile
+  cat <<EOF >> /etc/mongod.conf
+security:
+  keyFile: /etc/mongod-keyfile
+EOF
+fi
 
 /entrypoint.sh -f /etc/mongod.conf
