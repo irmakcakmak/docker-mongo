de2278ba36e58fd30adc5e73de5160bd670365e7
Erhan KESKEN <erhan.kesken@ebrandvalue.com>
2015-11-10 14:34:27 +0000

    fixed mongos configuration

--- 2.6/env-entrypoint.sh
+++ 2.6/env-entrypoint.sh
@@ -4,8 +4,15 @@ MONGO_CMD=mongod
 mkdir -p /data/db/$REPLICATION_SET_NAME/
 chown -R mongodb:mongodb /data/db/$REPLICATION_SET_NAME/
 
-if [ "z$REPLICATION_SET_NAME" != "z" ]; then
-  cat <<EOF >/etc/mongod.conf
+if [ "z$SHARDING_CONFIGDB" != "z" ]; then
+  MONGO_CMD=mongos
+  cat <<EOF >> /etc/mongod.conf
+sharding:
+  configDB: $SHARDING_CONFIGDB
+EOF
+else
+  if [ "z$REPLICATION_SET_NAME" != "z" ]; then
+    cat <<EOF >/etc/mongod.conf
 storage:
   dbPath: /data/db/$REPLICATION_SET_NAME/
 
@@ -13,12 +20,30 @@ replication:
   replSetName: $REPLICATION_SET_NAME
 
 EOF
-else
-  cat <<EOF >/etc/mongod.conf
+  else
+    cat <<EOF >/etc/mongod.conf
 storage:
   dbPath: /data/db/
 
 EOF
+  fi
+ 
+  if [ "z$KEY_FILE_CONTENT" != "z" ]; then
+    echo $KEY_FILE_CONTENT | tr ' ' '\n' > /etc/mongod-keyfile
+    chmod 600 /etc/mongod-keyfile
+    chown mongodb:mongodb /etc/mongod-keyfile
+    cat <<EOF >> /etc/mongod.conf
+security:
+  keyFile: /etc/mongod-keyfile
+EOF
+  fi
+  
+  if [ "z$CONFIGSVR" = "ztrue" ]; then
+    cat <<EOF >> /etc/mongod.conf
+sharding:
+  clusterRole: configsvr
+EOF
+  fi
 fi
 
 if [ "z$NET_PORT" != "z" ]; then
@@ -27,31 +52,6 @@ net:
    port: $NET_PORT
 EOF
 fi
-
-if [ "z$KEY_FILE_CONTENT" != "z" ]; then
-  echo $KEY_FILE_CONTENT | tr ' ' '\n' > /etc/mongod-keyfile
-  chmod 600 /etc/mongod-keyfile
-  chown mongodb:mongodb /etc/mongod-keyfile
-  cat <<EOF >> /etc/mongod.conf
-security:
-  keyFile: /etc/mongod-keyfile
-EOF
-fi
-
-if [ "z$CONFIGSVR" = "ztrue" ]; then
-  cat <<EOF >> /etc/mongod.conf
-sharding:
-  clusterRole: configsvr
-EOF
-fi
-
-if [ "z$SHARDING_CONFIGDB" != "z" ]; then
-  MONGO_CMD=mongos
-  cat <<EOF >> /etc/mongod.conf
-sharding:
-  configDB: $SHARDING_CONFIGDB
-EOF
-fi
-
+ 
 /entrypoint.sh $MONGO_CMD -f /etc/mongod.conf
 
