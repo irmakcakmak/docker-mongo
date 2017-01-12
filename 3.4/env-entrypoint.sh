cf61a03dd5eb6b15a987a62c8afce470d66fe1b1
Irmak Cakmak <irmak.cakmak@ebrandvalue.com>
2017-01-12 22:25:38 +0000

    Added 3.4 support

---
+++ 3.4/env-entrypoint.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+MONGO_CMD=mongod
+mkdir -p /data/db/$REPLICATION_SET_NAME/
+chown -R mongodb:mongodb /data/db/$REPLICATION_SET_NAME/
+
+if [ "z$SHARDING_CONFIGDB" != "z" ]; then
+  MONGO_CMD=mongos
+  cat <<EOF >> /etc/mongod.conf
+sharding:
+  configDB: $SHARDING_CONFIGDB
+EOF
+else
+
+  if [ "z$STORAGE_SMALLFILES" != "z" ]; then
+    STORAGE_SMALLFILES="smallFiles: true"
+  else
+    STORAGE_SMALLFILES="smallFiles: false"
+  fi
+
+  if [[ ! $STORAGE_ENGINE ]]; then
+    STORAGE_ENGINE="wiredTiger"
+  fi
+  echo "Storage engine is set to '$STORAGE_ENGINE'"
+  ENGINE="engine: $STORAGE_ENGINE"
+  if [[ $STORAGE_ENGINE == 'mmapv1' ]]; then
+    STORAGE_ENGINE="$STORAGE_ENGINE:
+                      $STORAGE_SMALLFILES"
+  else
+    STORAGE_ENGINE="$STORAGE_ENGINE:
+                      engineConfig:
+                      cacheSizeGB: $[MONGO_MEMORY/2-1024]
+                      journalCompressor: snappy
+                      directoryForIndexes: false"
+  fi
+
+  if [ "z$REPLICATION_SET_NAME" != "z" ]; then
+    cat <<EOF >/etc/mongod.conf
+storage:
+  dbPath: /data/db/$REPLICATION_SET_NAME/
+  $STORAGE_ENGINE
+
+replication:
+  replSetName: $REPLICATION_SET_NAME
+
+EOF
+  else
+    cat <<EOF >/etc/mongod.conf
+storage:
+  dbPath: /data/db/
+  $STORAGE_ENGINE
+EOF
+  fi
+ 
+  if [ "z$KEY_FILE_CONTENT" != "z" ]; then
+    echo $KEY_FILE_CONTENT | tr ' ' '\n' > /etc/mongod-keyfile
+    chmod 600 /etc/mongod-keyfile
+    chown mongodb:mongodb /etc/mongod-keyfile
+    cat <<EOF >> /etc/mongod.conf
+security:
+  keyFile: /etc/mongod-keyfile
+EOF
+  fi
+  
+  if [ "z$CONFIGSVR" = "ztrue" ]; then
+    cat <<EOF >> /etc/mongod.conf
+sharding:
+  clusterRole: configsvr
+EOF
+  fi
+fi
+
+if [ "z$NET_PORT" != "z" ]; then
+  cat <<EOF >> /etc/mongod.conf
+net:
+   port: $NET_PORT
+EOF
+fi
+
+
+/entrypoint.sh $MONGO_CMD -f /etc/mongod.conf
+
